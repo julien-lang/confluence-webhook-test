665f89fb65a9fe69d3d7df3b3b25648e3f863783
Julien Langlois <j.langlois@inuvika.com>
2017-04-15 17:22:08 -0700

    Improve receiver for inspecting incoming information

--- receiver.php
+++ receiver.php
@@ -1,3 +1,7 @@
 <?php
 
-die();
+$in = @file_get_contents('php://input');
+
+$data = @json_decode($in, true);
+
+file_put_contents('/tmp/webhook.log', json_encode($data, JSON_PRETTY_PRINT), FILE_APPEND);
